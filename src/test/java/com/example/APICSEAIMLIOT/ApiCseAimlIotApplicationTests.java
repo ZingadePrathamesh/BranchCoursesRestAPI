28eaf3783adfcc41d1ee124285766b49e36fd281
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2023-12-08 14:54:20 +0530

    first commit

---
+++ src/test/java/com/example/APICSEAIMLIOT/ApiCseAimlIotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APICSEAIMLIOT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCseAimlIotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
